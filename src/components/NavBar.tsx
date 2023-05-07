f59bd277d3f6601b142afc496bdf2c68cbdcd31c
jacksontriffon <jackson.triffon@gmail.com>
2023-05-07 17:00:04 +1000

    feat: add memopup icon to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-between px-4 pt-2 transition-all duration-300">
-      <div className="h-10 w-10 bg-neutral">
+    <div className="flex justify-between px-4 pt-4 transition-all duration-300">
+      <div className="h-12 w-12">
         <Image
+          src="/images/memopup.png"
           width={256}
           height={256}
-          src="../../public/Memopup.svg"
           alt="Memopup Logo"
         />
       </div>
