084a1a555440b09c5795df77c752605132dc2a68
jacksontriffon <jackson.triffon@gmail.com>
2023-05-20 00:52:55 +1000

    fix: comment out api endpoint

--- src/pages/api/gpt.ts
+++ src/pages/api/gpt.ts
@@ -14,32 +14,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<GenerateChatTitleResponseBody | { error: string }>
 ) {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method Not Allowed. Use GET instead." });
-    return;
-  }
-  const { message } = req.body as GenerateChatTitleRequestBody;
-  if (typeof message !== "string") {
-    res.status(400).json({ error: "Invalid message format" });
-    return;
-  }
-  try {
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content:
-            "Generate a title of a chat (Maximum of 3 words) with the following message: " +
-            message,
-        },
-      ],
-    });
-    const generatedTitle = response.data.choices[0];
-    const responseJSON = { title: generatedTitle?.message?.content ?? "" };
-    res.status(200).json(responseJSON);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
+  // if (req.method !== "POST") {
+  //   res.status(405).json({ error: "Method Not Allowed. Use GET instead." });
+  //   return;
+  // }
+  // const { message } = req.body as GenerateChatTitleRequestBody;
+  // if (typeof message !== "string") {
+  //   res.status(400).json({ error: "Invalid message format" });
+  //   return;
+  // }
+  // try {
+  //   const response = await openai.createChatCompletion({
+  //     model: "gpt-3.5-turbo",
+  //     messages: [
+  //       {
+  //         role: "user",
+  //         content:
+  //           "Generate a title of a chat (Maximum of 3 words) with the following message: " +
+  //           message,
+  //       },
+  //     ],
+  //   });
+  //   const generatedTitle = response.data.choices[0];
+  //   const responseJSON = { title: generatedTitle?.message?.content ?? "" };
+  //   res.status(200).json(responseJSON);
+  // } catch (error) {
+  //   console.error("Error:", error);
+  //   res.status(500).json({ error: "Something went wrong" });
+  // }
 }
